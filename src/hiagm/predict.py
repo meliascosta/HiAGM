2c4b76ef8fb29bed7f3026ed0964cdc5170435fc
Martin Elias Costa <meliascosta@gmail.com>
2022-11-29 19:53:49 -0300

    turn predictor into object

--- src/hiagm/predict.py
+++ src/hiagm/predict.py
@@ -20,50 +20,55 @@ from hiagm.helper.configure import Configure
 from hiagm.models.model import HiAGM
 
 
-def predict(text, config, model_checkpoint, max_labels=4):
-    """
-    Predict the labels of a text.
-    :param text: The text to be predicted.
-    :param config: The configuration of the model.
-    :param model_checkpoint: The path of the model checkpoint.
-    :param max_labels: The maximum number of labels to be predicted.
-    """
+class Predictor():
 
-    # Clean text 
-    data_str = preprocess_line(text)
-    
-    # loading corpus and generate vocabulary
-    corpus_vocab = Vocab(config,
-                         min_freq=5,
-                         max_size=50000)
+    def __init__(self, config, model_checkpoint) -> None:
+        # loading corpus and generate vocabulary
+        self.config = config
+        self.corpus_vocab = Vocab(config,
+                            min_freq=5,
+                            max_size=50000)
 
-    # build up model and load weights
-    checkpoint_model = torch.load(model_checkpoint, map_location=torch.device('cpu'))
-    hiagm = HiAGM(config, corpus_vocab, model_type=config.model.type, model_mode='TEST')
-    if config.model.quantize: 
-        hiagm =  torch.quantization.quantize_dynamic(
-        hiagm, {nn.LSTM, nn.Linear, nn.GRU, nn.RNN}, dtype=torch.qint8)
-        hiagm.load_state_dict(checkpoint_model)
-    else:
-        hiagm.load_state_dict(checkpoint_model['state_dict'])
-    hiagm.to(config.train.device_setting.device)
-    # define training objective & optimizer
-    collate_fn = Collator(config, corpus_vocab)
+        # build up model and load weights
+        checkpoint_model = torch.load(model_checkpoint, map_location=torch.device('cpu'))
+        self.hiagm = HiAGM(config, self.corpus_vocab, model_type=config.model.type, model_mode='TEST')
+        if config.model.quantize: 
+            self.hiagm =  torch.quantization.quantize_dynamic(
+            self.hiagm, {nn.LSTM, nn.Linear, nn.GRU, nn.RNN}, dtype=torch.qint8)
+            self.hiagm.load_state_dict(checkpoint_model)
+        else:
+            self.hiagm.load_state_dict(checkpoint_model['state_dict'])
+        self.hiagm.to(config.train.device_setting.device)
+        # define training objective & optimizer
+        self.collate_fn = Collator(config, self.corpus_vocab)
 
-    dataset = ClassificationDataset(config, corpus_vocab, corpus_lines=[data_str],mode='TEST')
-    data = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
-    mapper = pd.Series(corpus_vocab.i2v['label'])
-    one_example = next(iter(data))
-    hiagm.eval()
-    out = hiagm(one_example)
-    out = torch.sigmoid(out).cpu().tolist()
-    out_np = np.array(out)
-    out_np = pd.Series(out_np[0,:].flatten())
-    df = pd.concat([mapper, out_np], axis=1)
-    df.columns = ['label', 'score']
-    df_classes = df.sort_values(by='score', ascending=False).iloc[:max_labels]
-    print(list(df_classes.label), list(df_classes.score), list(df_classes.index))
-    return list(df_classes.label), list(df_classes.score), list(df_classes.index)
+
+    def predict(self, text, max_labels=4):
+        """Predict the text labels
+
+        Args:
+            text (str): Text to process
+            max_labels (int, optional): Maximum number of labels to predict. Defaults to 4.
+
+        Returns:
+            list: Predicted labels
+        """
+
+        # Clean text 
+        data_str = preprocess_line(text)
+        dataset = ClassificationDataset(self.config, self.corpus_vocab, corpus_lines=[data_str],mode='TEST')
+        data = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=self.collate_fn)
+        mapper = pd.Series(self.corpus_vocab.i2v['label'])
+        one_example = next(iter(data))
+        self.hiagm.eval()
+        out = self.hiagm(one_example)
+        out = torch.sigmoid(out).cpu().tolist()
+        out_np = np.array(out)
+        out_np = pd.Series(out_np[0,:].flatten())
+        df = pd.concat([mapper, out_np], axis=1)
+        df.columns = ['label', 'score']
+        df_classes = df.sort_values(by='score', ascending=False).iloc[:max_labels]
+        return list(df_classes.label), list(df_classes.score), list(df_classes.index)
     
     
 
@@ -82,5 +87,6 @@ if __name__ == "__main__":
 
     if not os.path.isdir(configs.train.checkpoint.dir):
         os.mkdir(configs.train.checkpoint.dir)
-
-    predict(text, configs, model_checkpoint)
+    p = Predictor(configs, model_checkpoint)
+    out = p.predict(text)
+    print(out)
